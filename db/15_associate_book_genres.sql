0772af13f62d194c109d864a1fae0cd7356abe00
Abdulmomen Jameli <abdulmomenjameli@outlook.com>
2025-04-15 14:44:21 +0100

    Feat: insert the book_id and genre_id into the books_genres table

--- db/15_associate_book_genres.sql
+++ db/15_associate_book_genres.sql
@@ -1,1 +1,19 @@
 \c my_bookshop
+
+INSERT INTO books_genres
+(book_id, genre_id)
+VALUES
+(1,1),
+(2,7),
+(3,4),
+(4,4),
+(4,1),
+(5,5),
+(6,8),
+(9,5),
+(10,6),
+(11,2),
+(12,3);
+
+
+SELECT * FROM books_genres;
