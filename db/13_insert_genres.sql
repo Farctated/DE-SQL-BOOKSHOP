4d071c3c190d93e787cdc7908cb6b01c6bbd4c69
Abdulmomen Jameli <abdulmomenjameli@outlook.com>
2025-04-15 10:52:06 +0100

    Feat: insert data into the genres table

--- db/13_insert_genres.sql
+++ db/13_insert_genres.sql
@@ -1,1 +1,20 @@
 \c my_bookshop
+
+INSERT INTO genres
+(genre)
+VALUES
+('science fiction'),
+('children''s'),
+('romance'),
+('fantasy'),
+('dystopian'),
+('science'),
+('adventure'),
+('classics');
+
+SELECT * FROM genres;
+
+
+
+
+
