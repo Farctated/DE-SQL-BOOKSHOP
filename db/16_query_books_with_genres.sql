ca56640bf81436748d60b57742515755726a3005
Abdulmomen Jameli <abdulmomenjameli@outlook.com>
2025-04-15 14:45:15 +0100

    Feat: add Query to retrieve the genre of the book, Emma

--- db/16_query_books_with_genres.sql
+++ db/16_query_books_with_genres.sql
@@ -1,7 +1,15 @@
 \c my_bookshop
 
 \echo '\n Here are the genres for Emma:\n'
-
+SELECT genre FROM books_genres
+JOIN books ON books_genres.book_id = books.book_id
+JOIN genres ON books_genres.genre_id = genres.genre_id
+WHERE books.title = 'Emma';
 
 
 \echo '\n Here are all the dystopian books:\n'
+
+SELECT title FROM books_genres
+JOIN books ON books_genres.book_id = books.book_id
+JOIN genres ON books_genres.genre_id = genres.genre_id
+WHERE genres.genre = 'dystopian';
