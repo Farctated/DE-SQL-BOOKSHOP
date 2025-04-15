6ef852c06aec68eddf0160c4158c2c413deeb390
Abdulmomen Jameli <abdulmomenjameli@outlook.com>
2025-04-15 15:07:35 +0100

    Feat: add query to retrieve number of books by each authors and average price of dystopian books

--- db/17_aggregate_queries.sql
+++ db/17_aggregate_queries.sql
@@ -2,6 +2,18 @@
 
 \echo '\n Here is the total number of books we have by each author:\n'
 
+SELECT author_name, COUNT(title) AS number_of_books FROM books
+JOIN authors
+ON authors.author_id = books.author_id
+GROUP BY author_name;
+
 
 
 \echo '\n Here is the average price for dystopian books:\n'
+
+SELECT * FROM books_genres;
+
+SELECT AVG(price_in_pence) AS average_price_of_dystopian_books FROM books_genres
+JOIN books ON books_genres.book_id = books.book_id
+JOIN genres ON books_genres.genre_id = genres.genre_id
+WHERE genres.genre = 'dystopian';
