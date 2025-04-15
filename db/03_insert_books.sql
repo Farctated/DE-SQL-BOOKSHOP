77e7e48e3f61bc6907a9e9a75d06dca111747bdb
Abdulmomen Jameli <abdulmomenjameli@outlook.com>
2025-04-15 10:33:35 +0100

    Fix: add all the books to the table

--- db/03_insert_books.sql
+++ db/03_insert_books.sql
@@ -6,8 +6,13 @@ VALUES
 ('The Hitchhiker''s Guide to the Galaxy', 899, 500, '1997-10-12', TRUE),
 ('The Little Prince', 699, 1020, '1943-04-06', TRUE),
 ('The Tale of Peter Rabbit', 599, 1000, '1902-10-01', TRUE),
+('Emma', 522, 390, '1815-12-23', TRUE),
+('Nineteen Eighty-Four: A Novel', 799, 420, '1949-06-08', TRUE),
+('The Handmaid''s Tale', 899, 10, '1985-08-01', TRUE),
+('Captain Corelli''s Mandolin', 999, 0, '1995-08-29', TRUE),
 ('A Brief History of Time', 825, 0, '1988-04-01', FALSE),
 ('The War of the Worlds', 250, 17, '1897-04-01', TRUE),
+('Pride and Prejudice', 699, 4, '1813-01-28', TRUE),
 ('A Very Recent Book', 123, 35, '2025-04-13', FALSE),
 ('Testing the thing', 1000, 37, '2005-11-23', FALSE);
 
